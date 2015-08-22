59be6e36b94d0d235acb5a9e0ece0146e3834e19
werdbrian <werdbrian@gmail.com>
2015-08-22 15:01:52 -0700

    Update Program.cs

--- Leblanc/Program.cs
+++ Leblanc/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using LeagueSharp;
 using LeagueSharp.Common;
+using ShineCommon;
+using SPrediction;
 using Color = System.Drawing.Color;
 
 #endregion
